bacdeb34432cf89ebaa2d3b8616fb14d50e62d0a
Jayadeep KM <kmjayadeep@gmail.com>
2022-12-10 23:18:11 +0530

    add firewall
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ hetzner-k3s/firewall.tf
@@ -0,0 +1,31 @@
+resource "hcloud_firewall" "k8s_firewall" {
+  name = "k8s_firewall"
+  rule {
+    direction = "in"
+    protocol  = "icmp"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+
+  rule {
+    direction = "in"
+    protocol  = "tcp"
+    port      = "6443"
+    source_ips = [
+      "0.0.0.0/0",
+      "::/0"
+    ]
+  }
+}
--- hetzner-k3s/main.tf
+++ hetzner-k3s/main.tf
@@ -13,18 +13,19 @@ data "hcloud_ssh_key" "ssh_key" {
 }
 
 resource "hcloud_server" "k8s1" {
-  name = "k8s1"
-  image = "ubuntu-2204"
+  name = "uranus"
+  image = "ubuntu-22.04"
   location  = "nbg1"
   server_type = "cx21"
   ssh_keys  = ["${data.hcloud_ssh_key.ssh_key.id}"]
   public_net {
-    ipv4_enabled = false
+    ipv4_enabled = true
     ipv6_enabled = true
   }
+  firewall_ids = [hcloud_firewall.k8s_firewall.id]
 }
 
 # Output Server Public IP address 
 output "server_ip_k8s1" {
- value = "${hcloud_server.k8s1.ipv6_address}"
+ value = "${hcloud_server.k8s1.ipv4_address}"
 }
