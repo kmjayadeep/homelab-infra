9fd641e9ee4e3d53e9daa68cdb5c317a7ca4dfe4
Jayadeep KM <kmjayadeep@gmail.com>
2023-05-12 12:48:59 +0200

    kubeconfig script
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

---
+++ pluto-ansible/kubeconfig.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# kubeconfig.sh: copy kubeconfig
+
+scp pluto:/etc/rancher/k3s/k3s.yaml /tmp
+
+# Remove existing config
+kubectl config delete-cluster cosmos
+kubectl config delete-context cosmos
+kubectl config delete-user cosmos
+
+# Import k3s config
+kubectl konfig import --save /tmp/k3s.yaml
+
+rm /tmp/k3s.yaml
+
