7d89e305d33c955af067753af5fc8793f498ad21
Jayadeep KM <kmjayadeep@gmail.com>
2023-10-08 10:46:59 +0200

    start docker
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- thinkpad-ansible/run.sh
+++ thinkpad-ansible/run.sh
@@ -2,4 +2,4 @@
 
 # run.sh: Run ansible playbook
 
-ansible-playbook playbook_hp.yaml -i hosts.ini -e @secrets.yml --vault-password-file <(pass homelab/hp/ansible-vault) -v
+ansible-playbook playbook_thinkpad.yaml -i hosts.ini -e @secrets.yml --vault-password-file <(pass homelab/hp/ansible-vault) -v
