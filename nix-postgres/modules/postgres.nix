68517d0d916c2fc472cdd4e779ea01c6742f213d
Jayadeep KM <kmjayadeep@gmail.com>
2024-07-30 08:21:10 +0200

    setup postgresql
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nix-postgres/modules/postgres.nix
+++ nix-postgres/modules/postgres.nix
@@ -1,3 +1,33 @@
 { config, lib, pkgs, ... }: {
 
+  services.postgresql = {
+    enable = true;
+    package = pkgs.postgresql_16;
+    ensureDatabases = [
+      "planka"
+      "totp"
+      "immich"
+    ];
+    ensureUsers = [
+      {
+        name = "planka";
+        ensureDBOwnership = true;
+      }
+      {
+        name = "totp";
+        ensureDBOwnership = true;
+      }
+      {
+        name = "immich";
+        ensureDBOwnership = true;
+      }
+    ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      #type database DBuser origin-address    auth-method
+      local all      all                      trust
+      # lan ipv4
+      host  all      all    192.168.1.1/24   trust
+    '';
+  };
+
 }
