baefbf9f37dc3d689f7b156db523573154cbc28e
Jayadeep KM <kmjayadeep@gmail.com>
2024-08-01 11:26:03 +0200

    add postgres backup
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nix-postgres/modules/postgres.nix
+++ nix-postgres/modules/postgres.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }: {
 
+  environment.systemPackages = with pkgs; [
+    restic
+  ];
+
   services.postgresql = {
     enable = true;
     enableTCPIP = true;
@@ -31,6 +35,8 @@
     '';
   };
 
+  environment.etc.restic-env.source = ../secrets/secret-restic.env;
+
   systemd.services.backup-postgres = {
     description = "Backup the Postgres db using restic";
     wants = ["network-online.target"];
@@ -44,8 +50,16 @@
     script = ''
       set -eu
       mkdir -p /tmp/postgres-backup
-      ${pkgs.postgresql_16}/bin/pg_dump totp > /tmp/postgres-backup/totp.sql
-      ${pkgs.postgresql_16}/bin/pg_dump planka > /tmp/postgres-backup/planka.sql
+      source /etc/restic-env
+      for DB_NAME in totp planka; do
+        ${pkgs.postgresql_16}/bin/pg_dump -d "''$DB_NAME" > "/tmp/postgres-backup/''${DB_NAME}_backup.sql"
+      done
+      ls /tmp/postgres-backup
+      ${pkgs.restic}/bin/restic backup /tmp/postgres-backup
+      ${pkgs.restic}/bin/restic forget --keep-monthly 12 --keep-last 10 --keep-daily 10 --prune
+      ${pkgs.restic}/bin/restic snapshots
+      mkdir -p /tmp/postgres-backup-prev
+      mv /tmp/postgres-backup/* /tmp/postgres-backup-prev
     '';
   };
 
