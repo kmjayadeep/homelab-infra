26b8a07fddad3082cb24a34894a1dc88d34707bc
Jayadeep KM <kmjayadeep@gmail.com>
2024-08-01 12:56:46 +0200

    fix postgres connection for immich
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nix-postgres/modules/postgres.nix
+++ nix-postgres/modules/postgres.nix
@@ -8,6 +8,7 @@
     enable = true;
     enableTCPIP = true;
     package = pkgs.postgresql_16;
+    extraPlugins = with pkgs.postgresql_16.pkgs; [ pgvector ];
     ensureDatabases = [
       "planka"
       "totp"
@@ -32,6 +33,8 @@
       local all      all                      trust
       # lan ipv4
       host  all      all    192.168.1.1/24    scram-sha-256
+      # Dockge hosts
+      host  all      all    172.25.0.0/16    scram-sha-256
     '';
   };
 
