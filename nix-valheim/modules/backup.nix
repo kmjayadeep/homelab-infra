91359efa10323c4b1f10af5f547bc234ed4908bd
Jayadeep KM <kmjayadeep@gmail.com>
2024-09-27 19:42:05 +0530

    add uptime ping for backups
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nix-valheim/modules/backup.nix
+++ nix-valheim/modules/backup.nix
@@ -21,6 +21,7 @@
       ${pkgs.restic}/bin/restic backup /var/lib/valheim/.config/unity3d/IronGate/Valheim/worlds_local
       ${pkgs.restic}/bin/restic forget --keep-monthly 12 --keep-last 10 --keep-daily 10 --prune
       ${pkgs.restic}/bin/restic snapshots
+      ${pkgs.curl}/bin/curl ''${${config.networking.hostName}_UPTIME_URL}
     '';
   };
 
