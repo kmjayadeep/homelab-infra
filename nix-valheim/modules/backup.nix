bcb4e027c8f6572a31f1563e818bac0764faf590
Jayadeep KM <kmjayadeep@gmail.com>
2024-09-28 06:58:39 +0530

    add restait wait lock
    Signed-off-by: Jayadeep KM <kmjayadeep@gmail.com>

--- nix-valheim/modules/backup.nix
+++ nix-valheim/modules/backup.nix
@@ -18,7 +18,7 @@
 
     script = ''
       source /etc/restic-env
-      ${pkgs.restic}/bin/restic backup /var/lib/valheim/.config/unity3d/IronGate/Valheim/worlds_local
+      ${pkgs.restic}/bin/restic backup /var/lib/valheim/.config/unity3d/IronGate/Valheim/worlds_local --retry-lock 10m
       ${pkgs.restic}/bin/restic forget --keep-monthly 12 --keep-last 10 --keep-daily 10 --prune
       ${pkgs.restic}/bin/restic snapshots
       ${pkgs.curl}/bin/curl ''${${config.networking.hostName}_UPTIME_URL}
